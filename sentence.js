bfe52e9eb8aa9b555933c184d63215156f90de26
Ongezwa <ongezwaf27@gmail.com>
2022-03-16 15:23:03 +0200

    third commit

--- sentence.js
+++ sentence.js
@@ -8,10 +8,33 @@ searchBTN.addEventListener("click", () => {
     var sentence = document.getElementById("searching").value;
     document.getElementById("displayField").innerHTML = sentence;
 
-    var wordLength = sentence.split(" ").length;
+    const wordLength = sentence.split(" ").length;
     document.getElementById('counter').innerHTML = wordLength;
 
     console.log(sentence)
+
+    let highlight = ""
+
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    const highlightedWords = wordLength.map((word) =>
+        word.length >= 4 ? `<span class="highlight">${word}</span>` : word
+    );
+    const joinedWords = highlightedWords.join(' ');
+
+    sentence.innerHTML = joinedWords;
+    console.log(joinedWords);
+
 
 
 })
