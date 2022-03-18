5711f6aaf2934c674938480eacda840c05386af0
Ongezwa <ongezwaf27@gmail.com>
2022-03-18 16:31:45 +0200

    longest word

--- functions.js
+++ functions.js
@@ -1,6 +1,26 @@
 function Main() {
-    
+    const getLongestWord = (words) => {
+        var words = document.getElementById("searching").value;
+        const longWord = words.split(" ")
+        let longestWord = "";
+        for (let index = 0; index < longWord.length; index++) {
+            if (longWord[index].length > longestWord.length) {
+                longestWord = longWord[index]
+                console.log(longestWord);
+            }
+        }
+        return longestWord
+    }
 
-
+    return {
+        getLongestWord
+    }
 
 }
+
+
+// const stringLength = longWord.map(longWord => longWord.length)
+        // const maxLength = Math.max(...stringLength)
+        // var results = longWord.find(longWord => longWord.length === maxLength)
+        // console.log(results);
+        // results.innerHTML = 
--- sentence.js
+++ sentence.js
@@ -1,5 +1,16 @@
 const searching = document.querySelector(".searching");
 const displayField = document.querySelector(".displayField");
+const hello = Main()
+/**
+ * hightlight words > 4
+ *  - highlight word > any word in the sentence
+ * - hide
+ */
+
+//  var checkbox2 = document.getElementById('select')
+//  checkbox2.addEventListener('change', () => {
+
+
 
 
 
@@ -28,21 +39,25 @@ searchBTN.addEventListener("click", () => {
 })
 // Hiding
 var checkbox = document.getElementById('hiding')
-checkbox.addEventListener('change', (shortWords) => {
+checkbox.addEventListener('change', () => {
 
     var sentence2 = document.getElementById("searching").value;
 
     const words2 = sentence2.split(" ");
 
-    const highlightedWords = words2.map((word) =>
-        word.length >= 4 ? `<span class="highlight">${word}</span>` : '' //word
+    const highlightedWords = words2.map((word) => word.length >= 4 ? `<span class="highlight">${word}</span>` : '' //word
     );
-    
+
     const joinedWords = highlightedWords.join(' ');
     document.getElementById("displayField").innerHTML = joinedWords;
 
     console.log(joinedWords);
 
 
-    // console.log(shortWords);
+})
+var checkbox2 = document.getElementById('select')
+checkbox2.addEventListener('change', () => {
+    var checkbox2 = document.querySelector("input[name='hide2']:checked");
+    document.getElementById("displayField").innerHTML = `<mark class = "highlight2">${hello.getLongestWord()}<mark/>` 
+    var checkbox2 = document.querySelector("input[name='hide2']");
 })
